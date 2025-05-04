e5df567c101b2834cc702c6e43f5afeecb5628e8
Dev-Alase <devalase5@gmail.com>
2025-05-04 12:44:05 +0530

    Initial commit: GraphX C++/CUDA skeleton

---
+++ README.md
@@ -0,0 +1,3 @@
+# GraphX C++/CUDA Version
+A framework for large-scale GPU-accelerated graph analytics.
+
---
+++ src/main.cu
@@ -0,0 +1,5 @@
+#include <iostream>
+int main() {
+ std::cout << "GraphX C++/CUDA Initialized!" << std::endl;
+ return 0;
+}
